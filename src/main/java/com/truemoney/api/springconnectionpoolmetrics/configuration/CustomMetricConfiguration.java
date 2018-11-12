5101b404896a042f70b7fb752a45a0e540c4fe2a
jameme123 <jetarin.v@ku.th>
2018-11-12 11:37:04 +0700

    add custom metric micrometer in order to fix bug of spring boot about trailing slash uri

---
+++ src/main/java/com/truemoney/api/springconnectionpoolmetrics/configuration/CustomMetricConfiguration.java
@@ -0,0 +1,40 @@
+package com.truemoney.api.springconnectionpoolmetrics.configuration;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
+import org.springframework.boot.actuate.metrics.web.servlet.DefaultWebMvcTagsProvider;
+import org.springframework.boot.actuate.metrics.web.servlet.WebMvcTagsProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+class CustomMetricConfiguration {
+
+    @Bean
+    public WebMvcTagsProvider webMvcTagsProvider() {
+        return new DefaultWebMvcTagsProvider() {
+            @Override
+            public Iterable<Tag> getTags(HttpServletRequest request, HttpServletResponse response, Object handler, Throwable exception) {
+                return Tags.of(super.getTags(request, response, handler, exception)).and(getRemovedTrailingSlashURI(request));
+            }
+        };
+    }
+
+    private Tag getRemovedTrailingSlashURI(HttpServletRequest request) {
+        String uri = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        if (uri == null) {
+            uri = request.getPathInfo();
+        }
+        if (!StringUtils.hasText(uri)) {
+            uri = "/";
+        }
+        uri = uri.replaceAll("//+", "/").replaceAll("/$", "");
+        return Tag.of("uri", uri.isEmpty() ? "root" : uri);
+    }
+
+}
